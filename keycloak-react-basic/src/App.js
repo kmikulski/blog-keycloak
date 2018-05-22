0fe98addd4ba780d743f44dcd1349424df3494b1
jakub.mikulski <jakub.mikulski@finnova.com>
2018-05-22 23:40:05 +0200

    components with keycloak authentication

--- keycloak-react-basic/src/App.js
+++ keycloak-react-basic/src/App.js
@@ -1,19 +1,24 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { BrowserRouter, Route, Link } from 'react-router-dom';
+import Welcome from './Welcome';
+import Secured from './Secured';
 import './App.css';
 
+
 class App extends Component {
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <BrowserRouter>
+        <div className="container">
+          <ul>
+            <li><Link to="/">public component</Link></li>
+            <li><Link to="/secured">secured component</Link></li>
+          </ul>
+          <Route exact path="/" component={Welcome} />
+          <Route path="/secured" component={Secured} />
+        </div>
+      </BrowserRouter>
     );
   }
 }
---
+++ keycloak-react-basic/src/Welcome.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Welcome extends Component {
+
+  render() {
+    return (
+      <div className="Welcome">
+        <p>This is your public-facing component.</p>
+      </div>
+    );
+  }
+}
+
+export default Welcome;
